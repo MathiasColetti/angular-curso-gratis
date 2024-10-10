62afac77600cab1dc008e4695706014e6e2832e7
Mathias Coletti <mathiasc2004@gmail.com>
2024-10-10 10:11:03 -0300

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {NavbarComponent} from "./navbar/navbar.component";
+import {FooterComponent} from "./footer/footer.component";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent, NavbarComponent, FooterComponent, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'serv';
+  exibeHome: boolean = true;
+
+  destruir() {
+    this.exibeHome = false;
+  }
 }
