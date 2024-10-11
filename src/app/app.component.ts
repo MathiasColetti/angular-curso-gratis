8c7a523197af28de1bb7030746b6d19a7cb5d05c
MathiasColetti <mathiasc2004@gmail.com>
2024-10-10 22:07:48 -0300

    NgContent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,13 @@ import {HomeComponent} from "./home/home.component";
 import {NavbarComponent} from "./navbar/navbar.component";
 import {FooterComponent} from "./footer/footer.component";
 import {NgIf} from "@angular/common";
+import { BaseUiComponent } from './base-ui/base-ui.component';
+import { UsersComponent } from './users/users.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, NavbarComponent, FooterComponent, NgIf],
+  imports: [RouterOutlet, HomeComponent, NavbarComponent, FooterComponent, NgIf, BaseUiComponent, UsersComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
