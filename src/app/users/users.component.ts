db1a700f56c29c531d789a6456009e8826ccf7f3
MathiasColetti <mathiasc2004@gmail.com>
2024-10-10 22:15:37 -0300

    NgFor

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,12 +1,14 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './users.component.html',
   styleUrl: './users.component.css'
 })
 export class UsersComponent {
+  users: string[] = ["Mathias", "Esther", "Dante"];
 
 }
